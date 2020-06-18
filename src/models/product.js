caecd10f775e2f2417aee61d0736d7274c6cc590
khai <ngovankhai2000@gmail.com>
2020-06-18 15:07:01 +0700

    db atlas

--- src/models/product.js
+++ src/models/product.js
@@ -18,10 +18,6 @@ var ProductSchema = new mongoose.Schema({
   },
   image:{
     type:String
-  },
-  countInStock:{
-    type: Number,
-    required:true
   }
 })
 const Product = mongoose.model('Product',ProductSchema)
